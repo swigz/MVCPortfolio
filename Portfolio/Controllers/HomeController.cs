21d8731eb0833d27f2a69eb76e2f407d17929ffe
DanielFerreira <danielferreira@bluetrendtech.com>
2018-04-20 17:58:25 +0100

    Initial Commit

---
+++ Portfolio/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿
+using System.Net;
+using System.Net.Mail;
+using System.Web.Mvc;
+using Portfolio.Models.Forms;
+
+namespace Portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendMessage(EmailFormModel model)
+        {
+
+            var fromAddress = new MailAddress("automailingportfolio@gmail.com", "Daniel Ferreira");
+            var toAddress = new MailAddress("daniel.sa.ferreira.7@gmail.com", "Daniel Ferreira");
+            const string fromPassword = "portfolio05";
+            const string subject = "Portfolio contact";
+            string body = "Nome: " + model.Name + " || Email: " + model.Email + " || Message: " + model.Message;
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp.Send(message);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Portfolio/Models/Forms/EmailFormModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Models.Forms
+{
+    public class EmailFormModel
+    {
+        [Required(ErrorMessage ="Please provide a name so that i can know who you are.")]
+        [Display(Name="Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Please provide an email so i can reach out if necessary.")]
+        [Display(Name="Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="Please type a message so that i know what this contact is about.")]
+        [Display(Name="Message")]
+        public string Message { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
